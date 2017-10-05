235bc81696002b5f3c66199edf35ae45dc4a22d1
GillesCallebaut <callebaut.gilles@gmail.com>
2017-10-05 17:32:35 +0200

    changed symbols for 915MHz

--- src/Comissioning_mydev.h
+++ src/Comissioning_mydev.h
@@ -19,7 +19,7 @@ Maintainer: Miguel Luis and Gregory Cristian
  * When set to 1 the application uses the Over-the-Air activation procedure
  * When set to 0 the application uses the Personalization activation procedure
  */
-#define OVER_THE_AIR_ACTIVATION                     1
+#define OVER_THE_AIR_ACTIVATION                     0
 
 /*!
  * Indicates if the end-device is to be connected to a private or public network
@@ -36,7 +36,7 @@ Maintainer: Miguel Luis and Gregory Cristian
  * The EUIs are used to identify all of the (virtual) parts of the LoRaWAN inside the backend server.
  *
  */
-#define LORAWAN_DEVICE_EUI                          { 0x00, 0xE6, 0xB1, 0x39, 0xE9, 0xD3, 0x4E, 0x30 }
+#define LORAWAN_DEVICE_EUI                          { 0x00, 0x80, 0x77, 0x31, 0x37, 0xB4, 0x8E, 0xE1 }
 /*!
  * 64 bit eapplication identifier, EUI-64 (unique)
  */
@@ -45,7 +45,7 @@ Maintainer: Miguel Luis and Gregory Cristian
 /*!
  * AES encryption/decryption cipher application key
  */
-#define LORAWAN_APPLICATION_KEY                     { 0xC7, 0x83, 0xFE, 0x54, 0xD4, 0x25, 0x0D, 0xCF, 0xF3, 0xEC, 0x02, 0x52, 0xF1, 0x6D, 0xED, 0x4B }
+#define LORAWAN_APPLICATION_KEY                     { 0x70, 0xB6, 0xB6, 0xE3, 0x2B, 0xF2, 0x05, 0x37, 0x37, 0x89, 0x2A, 0xAE, 0x79, 0xE0, 0xD7, 0x73 }
 /*!
  * Current network ID
  */
@@ -57,17 +57,17 @@ Maintainer: Miguel Luis and Gregory Cristian
  * If OTA is enabled this device will receive DevAddr from the network
  * When APB is selected, this value is hard-coded
  */
-#define LORAWAN_DEVICE_ADDRESS                      ( uint32_t )0x26011349
+#define LORAWAN_DEVICE_ADDRESS                      ( uint32_t )0x2601195F
 
 /*!
  * AES encryption/decryption cipher network session key
  * he NwkSKey is known by the network and the device and is used to validate the integrity of each message by its Message Integrity Code (MIC) (AES-CMAC)
  */
-#define LORAWAN_NWKSKEY                             { 0xF3, 0xB4, 0x38, 0x13, 0xE4, 0x1D, 0xF0, 0xEA, 0xD5, 0x18, 0x44, 0x38, 0x5B, 0xBE, 0xCE, 0xEB }
+#define LORAWAN_NWKSKEY                             { 0x75, 0x44, 0x5D, 0xCB, 0x6B, 0xD8, 0xA1, 0xD4, 0xC5, 0x25, 0x72, 0xC1, 0x46, 0xC4, 0x73, 0x3C }
 /*!
  * AES encryption/decryption cipher application session key
  * The AppSKey is used for end-to-end encryption of the frame payload.  (AES-128)
  */
-#define LORAWAN_APPSKEY                             { 0xB2, 0x35, 0xDD, 0xF9, 0x78, 0x38, 0x85, 0xF2, 0x94, 0xD2, 0x16, 0x97, 0xBE, 0x0B, 0x50, 0xBC }
+#define LORAWAN_APPSKEY                             { 0xA3, 0xC7, 0x8F, 0x27, 0x2A, 0x26, 0xF3, 0x9D, 0xD2, 0x59, 0x5D, 0x3A, 0x1F, 0x6D, 0xFE, 0x25 }
 
 #endif // __LORA_COMMISSIONING_H__
